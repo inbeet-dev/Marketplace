fa05082ce0917cfead985ba03f5b489520c101ff
hosein <mdoroud97@gmail.com>
2019-11-15 14:43:20 +0330

    add function for edit items of lumber list

--- app/Controllers/Http/LumberListController.js
+++ app/Controllers/Http/LumberListController.js
@@ -44,6 +44,41 @@ class LumberListController {
       success: true
     }
   }
+
+  async editItems({ request, response, auth }) {
+    const { projectId, items } = request.all()
+
+    const lumberList = await LumberList.findBy('project_id', projectId)
+
+    if (!lumberList)
+      throw new ServerException('lumber list of project not found')
+
+    const lumberListItems = await LumberListItem.query().where(
+      'lumber_list_id',
+      lumberList.id
+    )
+
+    for (let i = 0; i < lumberListItems.length; i++) {
+      const lumberListItem = await LumberListItem.find(lumberListItems[i].id)
+      lumberListItem.type = items[i].type
+      lumberListItem.meta = items[i].meta
+      await save(lumberListItem, response)
+    }
+
+    if (lumberListItems.length !== items.length) {
+      for (let i = lumberListItems.length; i < items.length; i++) {
+        const item = new LumberListItem()
+        item.type = items[i].type
+        item.meta = items[i].meta
+        item.lumber_list_id = lumberList.id
+        await save(item, response)
+      }
+    }
+
+    return {
+      success: true
+    }
+  }
 }
 
 module.exports = LumberListController
