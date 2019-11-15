d9175abd644ebfc71b63deccf5e6b2b92a9ab4ca
Sheila Nasehi <sheila.nasehi@gmail.com>
2019-11-15 14:39:28 +0330

    add edit profile route

--- start/routes.js
+++ start/routes.js
@@ -21,6 +21,7 @@ Route.group(() => {
   Route.post('login', 'UserController.login')
   Route.post('register', 'UserController.register')
   Route.get('dashboard', 'UserController.dashboard')
+  Route.put('', 'UserController.editProfile')
 }).prefix('api/v1/user')
 
 Route.group(() => {
