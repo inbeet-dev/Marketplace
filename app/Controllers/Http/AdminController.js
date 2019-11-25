3925dd85648650a8a25ed8f149a6ba78e25531ec
Sheila Nasehi <sheila.nasehi@gmail.com>
2019-11-26 00:39:15 +0330

    add admin auth

--- app/Controllers/Http/AdminController.js
+++ app/Controllers/Http/AdminController.js
@@ -1,7 +1,12 @@
 'use strict'
 
+const Auth = use('App/Utils/authenticate')
+const authenticate = new Auth()
+
 class AdminController {
-  getEmployees() {
+  async getEmployees({ response, auth }) {
+    await authenticate.admin(response, auth)
+
     return {
       success: true
     }
