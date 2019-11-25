fb5dd4ebad0bfca2a2aef9e592af486649867752
Sheila Nasehi <sheila.nasehi@gmail.com>
2019-11-26 00:22:21 +0330

    add validation body

--- app/Controllers/Http/AdminController.js
+++ app/Controllers/Http/AdminController.js
@@ -2,10 +2,26 @@
 
 const Auth = use('App/Utils/authenticate')
 const authenticate = new Auth()
+const { validate } = use('Validator')
+const ServerException = use('App/Exceptions/ServerException')
 
 class AdminController {
   async addEmployee({ request, response, auth }) {
     await authenticate.admin(response, auth)
+
+    const rules = {
+      name: 'required',
+      address: 'required',
+      email: 'required|email',
+      phone: 'required',
+      password: 'required',
+      role: 'required'
+    }
+
+    const validation = await validate(request.all(), rules)
+
+    if (validation.fails())
+      throw new ServerException(validation.messages(), 400)
 
     return {
       success: true
