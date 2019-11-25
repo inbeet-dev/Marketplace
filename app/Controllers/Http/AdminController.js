89cbcbc07a3d66960cac2ec9728677287a817b25
Sheila Nasehi <sheila.nasehi@gmail.com>
2019-11-26 00:45:22 +0330

    get all employees

--- app/Controllers/Http/AdminController.js
+++ app/Controllers/Http/AdminController.js
@@ -2,13 +2,23 @@
 
 const Auth = use('App/Utils/authenticate')
 const authenticate = new Auth()
+const User = use('App/Models/User')
 
 class AdminController {
   async getEmployees({ response, auth }) {
     await authenticate.admin(response, auth)
 
+    const admin = await auth.getUser()
+
+    const employees = await User.query()
+      .where('id', '!=', admin.id)
+      .fetch()
+
     return {
-      success: true
+      success: true,
+      data: {
+        employees
+      }
     }
   }
 }
