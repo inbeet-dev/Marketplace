91068e195dc3ab316ca131944596fd508e3f4330
hosein <mdoroud97@gmail.com>
2019-12-04 16:40:23 +0330

    get supplier by his id

--- app/Controllers/Http/SupplierController.js
+++ app/Controllers/Http/SupplierController.js
@@ -2,10 +2,25 @@
 
 const Auth = use('App/Utils/authenticate')
 const authenticate = new Auth()
+const ServerException = use('App/Exceptions/ServerException')
+const { validate } = use('Validator')
+const User = use('App/models/User')
 
 class SupplierController {
   async editSupplier({ response, request, auth }) {
     await authenticate.admin(response, auth)
+
+    const validation = await validate(request.all(), {
+      email: 'email',
+      supplierId: 'required'
+    })
+    if (validation.fails())
+      throw new ServerException(validation.messages(), 400)
+
+    const { supplierId } = request.all()
+
+    const supplier = await User.find(supplierId)
+    if (!supplier) throw new ServerException('Supplier not found')
 
     return {
       success: true
