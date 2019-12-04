a3bac06ca78e18b48ea06f6ab2d28cb6c0b35119
hosein <mdoroud97@gmail.com>
2019-12-04 17:12:30 +0330

    check validation of status

--- app/Controllers/Http/SupplierController.js
+++ app/Controllers/Http/SupplierController.js
@@ -4,6 +4,7 @@ const Auth = use('App/Utils/authenticate')
 const authenticate = new Auth()
 const { validate } = use('Validator')
 const ServerException = use('App/Exceptions/ServerException')
+const User = use('App/Models/User')
 
 class SupplierController {
   async changeStatus({ response, request, auth }) {
@@ -14,8 +15,20 @@ class SupplierController {
       status: 'required'
     })
 
+    const { status } = request.all()
+
     if (validation.fails())
       throw new ServerException(validation.messages(), 400)
+
+    if (
+      ![
+        User.STATUS.active,
+        User.STATUS.deActive,
+        User.STATUS.terminated,
+        User.STATUS.inReview
+      ].includes(status)
+    )
+      throw new ServerException('Role is invalid', 400)
 
     return {
       success: true
