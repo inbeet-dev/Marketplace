f71cdb3b19a751591d19d34ad6feb768fc3d9668
hosein <mdoroud97@gmail.com>
2019-12-04 16:18:02 +0330

    get all suppliers

--- app/Controllers/Http/SupplierController.js
+++ app/Controllers/Http/SupplierController.js
@@ -1,5 +1,22 @@
 'use strict'
 
-class SupplierController {}
+const Auth = use('App/Utils/authenticate')
+const authenticate = new Auth()
+const User = use('App/Models/User')
+
+class SupplierController {
+  async getSuppliers({ response, auth }) {
+    await authenticate.admin(response, auth)
+
+    const users = await User.query().where('role', 'supplier')
+
+    return {
+      success: true,
+      data: {
+        users
+      }
+    }
+  }
+}
 
 module.exports = SupplierController
