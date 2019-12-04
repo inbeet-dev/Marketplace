acfc418077038167bf7e880eaf753b5987f425eb
hosein <mdoroud97@gmail.com>
2019-12-04 16:44:45 +0330

    update all field of supplier

--- app/Controllers/Http/SupplierController.js
+++ app/Controllers/Http/SupplierController.js
@@ -2,6 +2,7 @@
 
 const Auth = use('App/Utils/authenticate')
 const authenticate = new Auth()
+const { save } = use('App/Utils/dbFunctions')
 const ServerException = use('App/Exceptions/ServerException')
 const { validate } = use('Validator')
 const User = use('App/models/User')
@@ -17,10 +18,23 @@ class SupplierController {
     if (validation.fails())
       throw new ServerException(validation.messages(), 400)
 
-    const { supplierId } = request.all()
+    const {
+      name,
+      address,
+      email,
+      phone: phoneNumber,
+      supplierId
+    } = request.all()
 
     const supplier = await User.find(supplierId)
     if (!supplier) throw new ServerException('Supplier not found')
+
+    if (name) supplier.name = name
+    if (address) supplier.meta = { ...supplier.meta, address }
+    if (email) supplier.meta = { ...supplier.meta, email }
+    if (phoneNumber) supplier.meta = { ...supplier.meta, phoneNumber }
+
+    await save(supplier, response)
 
     return {
       success: true
