9dcf63242e2e689e963441090df63bdaf4061630
hosein <mdoroud97@gmail.com>
2019-12-04 17:07:40 +0330

    check validation of request body

--- app/Controllers/Http/SupplierController.js
+++ app/Controllers/Http/SupplierController.js
@@ -2,10 +2,21 @@
 
 const Auth = use('App/Utils/authenticate')
 const authenticate = new Auth()
+const { validate } = use('Validator')
+const ServerException = use('App/Exceptions/ServerException')
 
 class SupplierController {
   async changeStatus({ response, request, auth }) {
     await authenticate.admin(response, auth)
+
+    const validation = await validate(request.all(), {
+      supplierId: 'required',
+      status: 'required'
+    })
+
+    if (validation.fails())
+      throw new ServerException(validation.messages(), 400)
+
     return {
       success: true
     }
