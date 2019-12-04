e911c2104863e7644be38ee2eaf65871ff922cb4
hosein <mdoroud97@gmail.com>
2019-12-04 14:22:09 +0330

    add register supplier

--- app/Controllers/Http/SupplierController.js
+++ app/Controllers/Http/SupplierController.js
@@ -1,5 +1,66 @@
 'use strict'
 
-class SupplierController {}
+const { validate } = use('Validator')
+const ServerException = use('App/Exceptions/ServerException')
+const User = use('App/Models/User')
+const { save } = use('App/Utils/dbFunctions')
+
+class SupplierController {
+  async register({ request, response, auth }) {
+    const {
+      name,
+      email,
+      password,
+      phonenumber,
+      lat,
+      long,
+      accountType
+    } = request.all()
+
+    const rules = {
+      email: 'required|email',
+      name: 'required',
+      password: 'required',
+      phonenumber: 'required',
+      lat: 'required',
+      long: 'required',
+      accountType: 'required'
+    }
+
+    const validation = await validate(request.all(), rules)
+    if (validation.fails())
+      throw new ServerException(validation.messages(), 400)
+
+    if (
+      ![User.ACCOUNT_TYPES.company, User.ACCOUNT_TYPES.individual].includes(
+        accountType
+      )
+    )
+      throw new ServerException('Invalid role', 400)
+
+    const meta = { phonenumber, lat, long, accountType }
+
+    const user = new User()
+
+    user.name = name
+    user.email = email
+    user.password = password
+    user.role = User.ROLES.supplier
+    user.meta = meta
+
+    await save(user, response)
+
+    const { token } = await auth.attempt(email, password)
+
+    return {
+      success: true,
+      data: {
+        token,
+        name,
+        role: user.role
+      }
+    }
+  }
+}
 
 module.exports = SupplierController
