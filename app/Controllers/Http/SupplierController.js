c2c28011686cd112a704548bb247d20cc5296491
hosein <mdoroud97@gmail.com>
2019-12-08 22:37:15 +0330

    get not bidded project

--- app/Controllers/Http/SupplierController.js
+++ app/Controllers/Http/SupplierController.js
@@ -29,10 +29,24 @@ class SupplierController {
       [supplier.id, Project.STATUS.open]
     )
 
+    const { rows: notBiddedProjects } = await Database.raw(
+      `SELECT projects.* FROM lumber_list_bids
+      INNER JOIN lumber_list_bid_items
+      ON lumber_list_bid_items.lumber_list_bid_id = lumber_list_bids.id
+      INNER JOIN lumber_list_items
+      ON lumber_list_items.id = lumber_list_bid_items.lumber_list_item_id
+      INNER JOIN lumber_lists
+      ON lumber_list_items.lumber_list_id = lumber_lists.id
+      INNER JOIN projects
+      ON projects.id = lumber_lists.project_id
+      WHERE supplier_id != 8 AND projects.status = 'Lumber List open'`
+    )
+
     return {
       success: true,
       data: {
-        bidedProjects
+        bidedProjects,
+        notBiddedProjects
       }
     }
   }
