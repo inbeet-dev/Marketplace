a9e238279b4fdfde4e21049dd05ab3266dcd56a6
hosein <mdoroud97@gmail.com>
2020-02-04 00:13:20 +0330

    add route

--- start/routes.js
+++ start/routes.js
@@ -83,6 +83,7 @@ Route.group(() => {
     'EstimatorAdminController.lumberListApprove'
   )
   Route.post('lumber-list-reject', 'EstimatorAdminController.lumberListReject')
+  Route.get('projects', 'EstimatorAdminController.projects')
 }).prefix('api/v1/estimator-admin')
 
 Route.group(() => {
