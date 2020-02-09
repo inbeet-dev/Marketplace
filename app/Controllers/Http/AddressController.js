1586337a6e22219e641bbd91a220c966ac052ef7
hosein <mdoroud97@gmail.com>
2020-02-09 19:26:24 +0330

    get location

--- app/Controllers/Http/AddressController.js
+++ app/Controllers/Http/AddressController.js
@@ -1,8 +1,28 @@
 'use strict'
 
+const { getLocation } = use('App/Mapquest/Function')
+
 class AddressController {
-  getLocation({ request }) {
-    return true
+  async getLocation({ request, response }) {
+    const { address } = request.all()
+
+    let location
+
+    try {
+      location = await getLocation(address)
+    } catch (err) {
+      return response.status(404).json({
+        error: 'ELOCATIONNOTFOUND',
+        message: 'location not found'
+      })
+    }
+
+    const { latitude: lat, longitude: long } = location[0]
+
+    return {
+      lat,
+      long
+    }
   }
 }
 
