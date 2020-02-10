4b5c8b72de701f0316cc2bb70d52c2eea76b54e7
mhmmdrzkh <mr.khkh1999@gmail.com>
2020-02-10 10:20:12 +0330

    last commit in 10/2/2020

--- resources/pages/estimator-admin/index.vue
+++ resources/pages/estimator-admin/index.vue
@@ -3,79 +3,225 @@
     <lumber-header
       :items="[
         { name: 'home', link: '/' },
-        { name: 'Projects', link: '/estimator-admin' }
+        { name: 'messages', link: 'message' }
       ]"
-      :user="$store.getters['User/getUser']"
     />
-    <v-row justify="center">
-      <v-col md="8" sm="10" cols="12" style="margin-top:150px;">
-        <h1>Estimator Manager Approval</h1>
+    <v-row justify="center" class="ma-0">
+      <v-col md="12" sm="12" cols="12" style="margin-top:150px;">
+        <h1 class="title">Estimator Manager Assignments</h1>
+      </v-col>
+      <v-col xl="2" lg="3" md="3" sm="3" cols="12">
+        <v-card class="filter-card px-5">
+          <span class="filter-card__title">Filter</span>
+          <v-list>
+            <v-list-item-group>
+              <v-list-item
+                v-for="(item, index) in filter"
+                :key="index"
+                @click="setShow(index)"
+              >
+                <v-list-item-content>
+                  <v-list-item-title :style="{ color: item.color }"
+                    >{{ index }}
+                    <span style="float:right" :style="{ color: item.color }">{{
+                      counts[index]
+                    }}</span></v-list-item-title
+                  >
+                </v-list-item-content>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+        </v-card>
+      </v-col>
+      <v-col xl="9" lg="9" md="8" sm="8" cols="12">
+        <v-card class="card">
+          <v-simple-table class="card__table">
+            <template v-slot:default>
+              <thead>
+                <tr>
+                  <th class="text-left card__table__header">User</th>
+                  <th class="text-left card__table__header">Address</th>
+                  <th class="text-left card__table__header">View Plan</th>
+                  <th class="text-left card__table__header">Lumber List</th>
+                  <th class="text-left card__table__header">Due Date</th>
+                  <th class="text-left card__table__header">Estimator</th>
+                  <th class="text-left card__table__header">status</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr
+                  v-for="(item, index) in showFilter"
+                  :key="index"
+                  class="card__table__row"
+                >
+                  <td class="card__table__row__item">{{ item.user.name }}</td>
+                  <td class="card__table__row__item">
+                    {{ item.project.address }}
+                  </td>
+                  <td class="card__table__row__item">
+                    <v-btn
+                      depressed
+                      dark
+                      color="#9ea4c4"
+                      class="card__table__row__item__btn"
+                      @click.stop="
+                        $store.dispatch('Dialog/show', {
+                          name: 'ViewPlanDialog'
+                        })
+                        getPlan(data[index].project.id)
+                      "
+                      >View Plan</v-btn
+                    >
+                  </td>
+                  <td class="card__table__row__item">
+                    <v-btn
+                      depressed
+                      dark
+                      color="#9ea4c4"
+                      class="card__table__row__item__btn"
+                      @click.stop="
+                        $store.dispatch('Dialog/show', {
+                          name: 'LumberList'
+                        }),
+                          getLumberList(data[index].project.id)
+                      "
+                      >Lumber List</v-btn
+                    >
+                  </td>
 
-        <v-card class="table-card">
-          <v-simple-table class="table">
-            <thead>
-              <tr>
-                <th class="text-left">User</th>
-                <th class="text-left">Project</th>
-                <th class="text-left">Lumber List</th>
-                <th class="text-left">Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="(item, index) in projects" :key="index">
-                <td class="item">{{ item.userName }}</td>
-                <td class="item">{{ item.projectName }}</td>
-                <td class="item">
-                  <button class="button button--grey" @click="show(index)">
-                    View
-                  </button>
-                </td>
-                <td class="item">
-                  <button
-                    class="button button--green"
-                    @click="approve(item.id)"
+                  <td class="card__table__row__item">
+                    <date-picker
+                      :due-date="item.project.dueDate"
+                      @click="save($event, index)"
+                    />
+                  </td>
+
+                  <td class="card__table__row__item">
+                    <v-select
+                      v-model="item.estimatorId"
+                      background-color="#f1f4f8"
+                      color="#9ea4c4"
+                      :items="estimators"
+                      solo
+                      @change="assing(index)"
+                    ></v-select>
+                  </td>
+                  <td
+                    class="card__table__row__item"
+                    :style="{ color: filter[item.project.status].color }"
                   >
-                    Approve</button
-                  ><button
-                    class="button button--red"
-                    style="margin-left: 20px"
-                    @click="reject(item.id)"
-                  >
-                    Reject
-                  </button>
-                </td>
-              </tr>
-            </tbody>
+                    {{ item.project.status }}
+                  </td>
+                </tr>
+              </tbody>
+            </template>
           </v-simple-table>
         </v-card>
       </v-col>
+      <view-plan-dialog :data="plans" />
+      <lumber-list :lumberList="lumberList" />
     </v-row>
-    <lumber-list-viewer />
   </v-container>
 </template>
 
 <script>
-import LumberHeader from '@/components/Header.vue'
-import LumberListViewer from '@/components/Shared/LumberListViewer'
-
+import * as moment from 'moment'
+import LumberHeader from '../../components/Header'
+import DatePicker from '../../components/Shared/DatePicker'
+import ViewPlanDialog from '../../components/ViewPlanDialog'
+import LumberList from '../../components/LumberList'
 export default {
   components: {
     LumberHeader,
-    LumberListViewer
+    DatePicker,
+    ViewPlanDialog,
+    LumberList
   },
-  data() {
-    return {
-      projects: null
-    }
-  },
+  data: () => ({
+    status: {
+      notAssigned: { text: 'Not Assigned', color: '#f78f1e' },
+      assigned: { text: 'Assigned', color: '#3ce057' }
+    },
+    filter: {
+      All: { color: '#000000' },
+      'Lumber List open': { color: '#f78f1e' },
+      'Lumber List Completed': { color: '#3ce057' }
+      // { text: 'Completed', color: '' },
+      // { text: 'Closed', color: '' },
+      // { text: 'Waiting', color: '' }
+    },
+    counts: {},
+    data: [],
+    items: ['Name Estimator'],
+    estimators: [],
+    plans: [],
+    lumberList: {},
+    show: '',
+    showFilter: []
+  }),
   async mounted() {
     await this.$store.restored
-    this.loadProjects()
+    this.data = (await this.$axios.get('/api/v1/estimator-admin/projects', {
+      headers: {
+        Authorization: `Bearer ${this.$store.getters['Auth/getToken']}`
+      }
+    })).data
+    this.showFilter = this.data
+    this.counts.All = this.data.length
+    this.data.forEach((element) => {
+      if (!this.counts[element.project.status]) {
+        this.counts[element.project.status] = 0
+      }
+      this.counts[element.project.status]++
+    })
+    this.counts = { ...this.counts }
+    console.log(this.counts)
+    const estimators = (await this.$axios.get(
+      '/api/v1/estimator-admin/estimators',
+      {
+        headers: {
+          Authorization: `Bearer ${this.$store.getters['Auth/getToken']}`
+        }
+      }
+    )).data
+    estimators.forEach((element) => {
+      this.estimators.push({ text: element.name, value: element.id })
+    })
   },
   methods: {
-    async loadProjects() {
-      this.projects = (await this.$axios.get(
-        '/api/v1/estimator-admin/lumber-lists',
+    save($event, index) {
+      this.data[index].date = $event
+      this.$axios.put(
+        '/api/v1/estimator-admin/due-date',
+        {
+          projectId: this.data[index].project.id,
+          dueDate: this.data[index].date
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${this.$store.getters['Auth/getToken']}`
+          }
+        }
+      )
+    },
+    assing(index) {
+      this.$axios.put(
+        '/api/v1/project/assign',
+        {
+          projectId: this.data[index].project.id,
+          estimatorId: this.data[index].estimatorId
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${this.$store.getters['Auth/getToken']}`
+          }
+        }
+      )
+    },
+    async getLumberList(id) {
+      await this.$store.restored
+      this.lumberList = (await this.$axios.get(
+        `/api/v1/estimator-admin/lumber-list/${id}`,
         {
           headers: {
             Authorization: `Bearer ${this.$store.getters['Auth/getToken']}`
@@ -83,78 +229,121 @@ export default {
         }
       )).data
     },
-    show(index) {
-      this.$store.dispatch('Dialog/show', {
-        name: 'LumberListViewer',
-        data: this.projects[index].items
+    async getPlan(id) {
+      const temp = []
+      this.plans = await this.$axios.get(
+        `/api/v1/estimator-admin/files?projectsId[]=2`,
+        {
+          headers: {
+            Authorization: `Bearer ${this.$store.getters['Auth/getToken']}`
+          }
+        }
+      )
+      this.plans.data.forEach(function(element) {
+        if (element.project_id === id) {
+          temp.push(element)
+        }
       })
+      this.plans = temp
     },
-    async approve(id) {
-      await this.$axios.post(
-        '/api/v1/estimator-admin/lumber-list-approve',
-        { lumberListId: id },
-        {
-          headers: {
-            Authorization: `Bearer ${this.$store.getters['Auth/getToken']}`
-          }
-        }
-      )
-      this.$store.dispatch('SnackBar/show', 'Lumber List approved')
+    setShow(index) {
+      this.show = index
     },
-    async reject(id) {
-      await this.$axios.post(
-        '/api/v1/estimator-admin/lumber-list-reject',
-        { lumberListId: id },
-        {
-          headers: {
-            Authorization: `Bearer ${this.$store.getters['Auth/getToken']}`
+    moment(...arg) {
+      return moment(...arg)
+    }
+  },
+  watch: {
+    show() {
+      this.showFilter = []
+      if (this.show !== 'All') {
+        this.data.forEach((element) => {
+          if (this.show === element.project.status) {
+            this.showFilter.push(element)
           }
-        }
-      )
-      this.$store.dispatch('SnackBar/show', 'Lumber List rejected')
+        })
+      }
+      if (this.show === 'All') {
+        this.showFilter = this.data
+      }
     }
   }
 }
 </script>
+
 <style lang="scss" scoped>
-h1 {
-  text-align: center;
-  line-height: 70px;
-  margin-bottom: 50px;
-}
-.table-card {
-  margin-top: 10px;
-  padding: 0 !important;
-  width: 100%;
-  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.16);
-  border-radius: 10px;
-  overflow: hidden;
-}
-.table-card .table th {
-  border-bottom: none !important;
-  background-color: #fafafa;
-  color: #9ea4c4;
-}
-
-.button {
-  padding: 5px 20px;
-  border-radius: 5px;
-  font-size: 15px;
-  min-width: 100px;
-
-  &--grey {
-    background-color: #9ea3c4;
-    color: #ffffff;
+.container {
+  min-height: 100vh;
+  .title {
+    text-align: center;
+    font-weight: 600;
+    font-size: 30px !important;
+    line-height: 70px;
   }
-
-  &--green {
-    background-color: #d8f9dd;
-    color: #3ce057;
+  .filter-card {
+    border-top: 5px solid #f78f1e;
+    border-radius: 5px;
+    box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.16);
+    &__title {
+      color: #000000;
+      display: block;
+      text-align: center;
+      font-size: 20px;
+      font-weight: 500;
+      line-height: 50px;
+    }
   }
+  .card {
+    box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.16);
+    border-radius: 10px;
+    overflow: hidden;
+    &__table {
+      &__header {
+        border-bottom: none !important;
+        color: #9ea4c4;
+        background-color: #fafafa;
+      }
+      &__row {
+        &__item {
+          border-bottom: none !important;
 
-  &--red {
-    background-color: #fbd6d6;
-    color: #ec3434;
+          &__btn {
+            border-radius: 8px;
+            text-transform: capitalize;
+            transition: 0.3s;
+            &:hover {
+              background-color: #f78f1e !important;
+            }
+          }
+          &::v-deep .v-text-field__details {
+            display: none;
+          }
+          &::v-deep .v-input__control {
+            border-radius: 8px;
+          }
+          &::v-deep .v-input {
+            max-width: 200px;
+          }
+          &::v-deep .v-input__slot {
+            margin: 10px 0;
+            box-shadow: none !important;
+          }
+        }
+
+        &__item:first-child {
+          border-left: 5px solid #f78e1e00;
+        }
+      }
+      &__row:hover {
+        box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.16);
+        .card__table__row__item:first-child {
+          border-left: 5px solid #f78e1e;
+        }
+        .card__table__row__item {
+          background-color: #ffffff;
+        }
+      }
+    }
   }
 }
 </style>
